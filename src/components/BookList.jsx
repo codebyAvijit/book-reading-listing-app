529a304074537398c948e94b99e3556b6ec2f4d1
codeByAvijit <deviljit.ap@gmail.com>
2025-09-10 15:01:49 +0530

    edit functionality implemented

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,6 +1,29 @@
 import React from "react";
 
-const BookList = ({ booksArray, setBooksArray }) => {
+const BookList = ({
+  booksArray,
+  setBooksArray,
+  startEditing,
+  editingId,
+  editingBook,
+  setEditingBook,
+  setEditingId,
+}) => {
+  const onSaveHandle = () => {
+    setBooksArray((prev) => {
+      const updated = prev.map((b) =>
+        b.id === editingId ? { ...b, ...editingBook } : b
+      );
+      localStorage.setItem("booksArray", JSON.stringify(updated));
+      return updated;
+    });
+    setEditingId(null);
+  };
+
+  const onCancelHandle = () => {
+    setEditingId(null);
+    setEditingBook({ title: "", author: "", category: "", status: "" });
+  };
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-10">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -24,36 +47,112 @@ const BookList = ({ booksArray, setBooksArray }) => {
           </tr>
         </thead>
         <tbody>
-          {booksArray.map((singleBookDetail) => (
-            <tr
-              key={singleBookDetail.id}
-              className="odd:bg-white even:bg-gray-50"
-            >
-              <td className="px-6 py-4">{singleBookDetail.title}</td>
-              <td className="px-6 py-4">{singleBookDetail.author}</td>
-              <td className="px-6 py-4">{singleBookDetail.category}</td>
-              <td className="px-6 py-4">{singleBookDetail.status}</td>
-              <td className="px-6 py-4">
-                <div className="flex space-evenly gap-2">
-                  <button className="px-2 py-1 text-xs text-white bg-blue-500 rounded hover:bg-blue-600">
-                    Edit
-                  </button>
-                  <button
-                    onClick={() =>
-                      setBooksArray(
-                        booksArray.filter((book) => {
-                          return book.id !== singleBookDetail.id;
-                        })
-                      )
-                    }
-                    className="px-2 py-1 text-xs text-white bg-red-500 rounded hover:bg-red-600"
-                  >
-                    Delete
-                  </button>
-                </div>
-              </td>
-            </tr>
-          ))}
+          {booksArray.map((singleBookDetail) =>
+            editingId === singleBookDetail.id ? (
+              <tr
+                key={singleBookDetail.id}
+                className="odd:bg-white even:bg-gray-50"
+              >
+                <td className="px-6 py-4">
+                  <input
+                    type="text"
+                    value={editingBook.title}
+                    onChange={(e) => {
+                      setEditingBook({ ...editingBook, title: e.target.value });
+                    }}
+                  />
+                </td>
+                <td className="px-6 py-4">
+                  <input
+                    type="text"
+                    value={editingBook.author}
+                    onChange={(e) => {
+                      setEditingBook({
+                        ...editingBook,
+                        author: e.target.value,
+                      });
+                    }}
+                  />
+                </td>
+                <td className="px-6 py-4">
+                  <input
+                    type="text"
+                    value={editingBook.category}
+                    onChange={(e) => {
+                      setEditingBook({
+                        ...editingBook,
+                        category: e.target.value,
+                      });
+                    }}
+                  />
+                </td>
+                <td className="px-6 py-4">
+                  <input
+                    type="text"
+                    value={editingBook.status}
+                    onChange={(e) => {
+                      setEditingBook({
+                        ...editingBook,
+                        status: e.target.value,
+                      });
+                    }}
+                  />
+                </td>
+                <td className="px-6 py-4">
+                  <div className="flex space-evenly gap-2">
+                    <button
+                      onClick={() => {
+                        onSaveHandle();
+                      }}
+                      className="px-2 py-1 text-xs text-white bg-blue-500 rounded hover:bg-blue-600"
+                    >
+                      save
+                    </button>
+                    <button
+                      onClick={() => {
+                        onCancelHandle();
+                      }}
+                      className="px-2 py-1 text-xs text-white bg-red-500 rounded hover:bg-red-600"
+                    >
+                      cancel
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ) : (
+              <tr
+                key={singleBookDetail.id}
+                className="odd:bg-white even:bg-gray-50"
+              >
+                <td className="px-6 py-4">{singleBookDetail.title}</td>
+                <td className="px-6 py-4">{singleBookDetail.author}</td>
+                <td className="px-6 py-4">{singleBookDetail.category}</td>
+                <td className="px-6 py-4">{singleBookDetail.status}</td>
+                <td className="px-6 py-4">
+                  <div className="flex space-evenly gap-2">
+                    <button
+                      onClick={() => startEditing(singleBookDetail.id)}
+                      className="px-2 py-1 text-xs text-white bg-blue-500 rounded hover:bg-blue-600"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() =>
+                        setBooksArray(
+                          booksArray.filter((book) => {
+                            return book.id !== singleBookDetail.id;
+                          })
+                        )
+                      }
+                      className="px-2 py-1 text-xs text-white bg-red-500 rounded hover:bg-red-600"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            )
+          )}
         </tbody>
       </table>
     </div>
--- src/components/BookListApp.jsx
+++ src/components/BookListApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 import AddBookForm from "./AddBookForm";
 import BookList from "./BookList";
 
@@ -7,6 +7,17 @@ const BookListApp = () => {
   const [booksArray, setBooksArray] = useState(() => {
     const storedBooks = localStorage.getItem("booksArray");
     return storedBooks ? JSON.parse(storedBooks) : [];
+  });
+
+  //editing useStates for editing functionality
+
+  const [editingId, setEditingId] = useState(null);
+
+  const [editingBook, setEditingBook] = useState({
+    title: "",
+    author: "",
+    category: "",
+    status: "",
   });
 
   // parent handler
@@ -18,11 +29,29 @@ const BookListApp = () => {
     });
   };
 
+  //creating a function to start editing a book by finding one
+
+  const startEditing = (id) => {
+    const findBookToBeEdited = booksArray.find((bookFound) => {
+      return bookFound.id === id;
+    });
+    setEditingId(id);
+    setEditingBook({ ...findBookToBeEdited });
+  };
+
   return (
     <div>
       <h1 className="text-center">BookList App</h1>
       <AddBookForm addBookHandler={addBookHandler} />
-      <BookList booksArray={booksArray} setBooksArray={setBooksArray} />
+      <BookList
+        booksArray={booksArray}
+        setBooksArray={setBooksArray}
+        editingId={editingId}
+        editingBook={editingBook}
+        startEditing={startEditing}
+        setEditingBook={setEditingBook}
+        setEditingId={setEditingId}
+      />
     </div>
   );
 };
